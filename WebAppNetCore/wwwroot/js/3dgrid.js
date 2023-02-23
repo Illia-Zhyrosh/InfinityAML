d3dd8841ba0c312784d9a6abfadb2fcf96cf5ba1
Admin <Admin@DESKTOP-6QB92EP>
2023-02-23 19:46:51 +0100

    Add project files.

---
+++ WebAppNetCore/wwwroot/js/3dgrid.js
@@ -0,0 +1,205 @@
+﻿import * as THREE from './three/build/three.module.js';
+import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
+import { ColladaLoader } from './three/examples/jsm/loaders/ColladaLoader.js';
+
+function init() {
+    const cont = document.getElementById('canv');
+    var width = cont.clientWidth - 50;
+    var height = cont.clientHeight - 50;
+    var scene = new THREE.Scene();
+    const loader = new ColladaLoader();
+    var camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 500);
+   
+    var renderer = new THREE.WebGLRenderer({
+        antialias: false
+    });
+    //Resizing renderer when window size changed;
+    function resize() {
+        if (window.innerHeight / 2 < height) {
+            height = (window.innerHeight / 2) - 50;
+        }
+        else if (window.innerWidth / 2 < width) {
+            width = (window.innerWidth / 2) - 50;
+        }
+        else {
+            width = cont.clientWidth - 50;
+            height = cont.clientHeight - 50;
+        }
+        renderer.setSize(width, height);
+        renderer.setPixelRatio(1);
+    }
+    
+    
+    resize();
+    const axisHelper = new THREE.AxesHelper(planeH);
+    scene.add(axisHelper);
+
+    const rendererDom = renderer.domElement;
+    rendererDom.className += 'renderer';
+    cont.appendChild(rendererDom);
+    scene.background = new THREE.Color('White');
+    camera.position.set(6, 6, 0);
+    camera.lookAt(0, 0, 0);
+    //Grid peculiarities
+    var numW = 16;
+    var numH = 16;
+    var planeW = 32;
+    var planeH = 32;
+    var plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(planeW , planeH , numW, numH),
+        new THREE.MeshBasicMaterial(
+            {
+                color: new THREE.Color('White'),
+                side: THREE.DoubleSide,
+                wireframe: false,
+                visible: false,
+                
+            })
+    );
+    plane.geometry.frustumCulled = false;
+    plane.rotateX(Math.PI / 2);
+    plane.name = 'Grid';
+    scene.add(plane);
+   
+    const gridHelper = new THREE.GridHelper(planeW, planeH, new THREE.Color('Black'), new THREE.Color('DarkGray'));
+    gridHelper.geometry.computeBoundingBox();
+    gridHelper.frustumCulled = false;
+    gridHelper.position.set(0.00001, 0, 0.00001);
+    
+    scene.add(gridHelper);
+    const highlightMesh = new THREE.Mesh(
+        new THREE.PlaneGeometry(1, 1),
+        new THREE.MeshBasicMaterial({
+            side: THREE.DoubleSide,
+            color: new THREE.Color('Red')
+        })
+    );
+    highlightMesh.rotateX(Math.PI / 2);
+    // Square highlighting 
+    function Placement() {
+        
+        
+        
+        highlightMesh.position.set(0.5, 0, 0.5);
+        scene.add(highlightMesh);
+        const mousePos = new THREE.Vector2();
+        const rayCaster = new THREE.Raycaster();
+        
+        let intersection;
+        var highlighterPos;
+        cont.addEventListener('mousemove', function (e) {
+            const x = e.clientX - cont.getBoundingClientRect().left;
+            const y = e.clientY - cont.getBoundingClientRect().top;
+            
+            mousePos.x = (x / cont.clientWidth) * 2 - 1;
+            mousePos.y = - (y / cont.clientHeight) * 2 + 1;
+            //console.log(mousePos);
+            rayCaster.setFromCamera(mousePos, camera);
+            intersection = rayCaster.intersectObjects(scene.children);
+            intersection.forEach(function (intersect) {
+                if (intersect.object.name === 'Grid') {
+                    highlighterPos = new THREE.Vector3().copy(intersect.point).floor().addScalar(0.5);
+                    highlightMesh.position.set(highlighterPos.x, 0, highlighterPos.z);
+                }
+            });
+        });
+        $(cont).one('click', function () {
+            loadMod();
+            
+        });
+        //Loading model and placing it in place of highlighter
+        function loadMod() {
+            var xpos = highlighterPos.x;
+            var ypos = 0;
+            var zpos = highlighterPos.z;
+            console.log(highlightMesh.position.x + ' ' + highlightMesh.position.y + ' ' + highlightMesh.position.z);
+            console.log(xpos + ' ' + ypos + ' ' + zpos);
+           
+
+            var modelPath = fetch('./wwwroot/models/Av650.dae', { mode: 'cors' }).then(response => {
+                loader.load(response.url, function (loaded) {
+                    var model = loaded.scene;
+                    model.name = 'Added';
+                    model.position.set(xpos, ypos, zpos);
+                    model.scale.set(0.5, 0.5, 0.5);
+                    model.rotateX(Math.PI / 2);
+                    scene.add(model);
+                })
+                
+                
+            }
+            );
+            
+            
+            
+            
+            scene.remove(highlightMesh);
+            
+            
+            
+        }
+       
+        
+    }
+    
+
+
+    //TODO make function to add selectable models;
+
+    //ItemFinder
+    $('#tree').children().each(function () {
+
+        var items = document.getElementsByClassName('itemSelectable');
+        $(items).each(function () {
+            ($(this).click(Placement));
+        })
+    });
+    
+            
+        
+    const clearButt = document.getElementById('clearData');
+    clearButt.addEventListener('click', function () {
+
+        var children = scene.children;
+        $(children).each(function () {
+            if (this.name === 'Added') {
+                scene.remove(this);
+            }
+        })
+
+    });
+    
+    
+
+    
+    let resized = false;
+    //Adding ambient lighting
+    const pointLight = new THREE.AmbientLight(0x404040);
+    scene.add(pointLight);
+    // Camera movement
+    const control = new OrbitControls(camera, renderer.domElement);
+    control.maxPolarAngle = Math.PI / 8;
+    control.minDistance = 6.3;
+    control.maxDistance = 16;
+    //Resizing events
+    window.addEventListener('resize', function () {
+        resized = true;
+        
+    })
+    function animate() {
+        if (resized) { resize(); }
+        requestAnimationFrame(animate);
+        control.update();
+        
+        renderer.render(scene, camera);
+
+    }
+    
+    
+    animate();
+};
+
+init();
+
+
+
---
+++ WebAppNetCore/wwwroot/js/treeview.js
@@ -0,0 +1,394 @@
+﻿
+//list init;
+var li = document.createElement('li');
+$('#tree').append(li);
+var a = document.createElement('a');
+a.text = 'Machinery';
+a.className = 'itemExpandable';
+li.style.listStyle = 'none';
+li.append(a);
+var sub_ul = document.createElement('ul');
+li.append(sub_ul);
+var arrayCollection;
+
+// Making a nested list from xml file
+// caching disabled to allow data update inside xml;
+$.ajax({
+    url: '/lib/data.xml',
+    dataType: 'xml',
+    cache: false,
+    success: function (data) {
+        $(data).find('TreeViewItemHead').each(function ()
+        {
+            
+            var t = isParent(this);
+            if (t == true) {
+                var p = whoIsParent($(this));
+                var parent = $(p).attr('Text');
+                if (parent == 'Machinery') {
+
+                    var header = document.createElement('li');
+                    header.style.margin = '5px';
+
+                    var at = document.createElement('a');
+                    at.text = ($(this).attr('Text'))
+                    at.className = 'itemExpandable';
+
+                    header.style.listStyle = 'none';
+                    header.append(at);
+                    
+                    sub_ul.append(header);
+                    lastElem = header;
+                    
+                    
+                }
+                else {
+                    var subul = document.createElement('ul');
+
+                    var head = document.createElement('li');
+                    head.style.listStyle = 'none';
+                    head.style.margin = '5px';
+                    subul.append(head);
+
+                    var innertext = document.createElement('a');
+                    innertext.text = $(this).attr('Text')
+                    innertext.className = 'itemExpandable';
+                    
+                    head.append(innertext);
+
+                    lastElem.append(subul);
+                    
+                }
+            }
+            
+            
+            
+            
+            
+        })
+        $(data).find('TreeViewItem').each(function () {
+            var p = whoIsParent(this);
+            var atext = $(this).attr('Text');
+            var a = document.getElementsByTagName('a');
+            var head;
+            $(a).each(function () {
+                if ($(p).attr('Text') == this.text) {
+                    
+                    head = $(this).parent();
+                    var ul = $('<ul/>').appendTo(head);
+                    var li = document.createElement('li');
+                    li.style.listStyle = 'none';
+                    
+                    li.className = 'itemSelectableParent';
+                    //li.onclick = itemSelected;
+                    
+                    ul.append(li);
+                    var val = document.createElement('a');
+                    val.text = atext;
+                    val.className = 'itemSelectable';
+                    val.onclick = itemSelected;
+                    li.append(val);
+                    
+                    li.style.display = 'none';
+                    var butt = document.createElement('button');
+                    butt.textContent = 'info';
+                    butt.className = 'infoButtons';
+                    butt.addEventListener('click', function () {
+                        getDataByName(this.previousSibling.textContent);
+                        
+                    })
+                    li.append(butt);
+                }
+            });
+            
+        })
+        var expandables = document.querySelectorAll('.itemExpandable');
+        expandables.forEach(
+            expandable => {
+                expandable.addEventListener('click',
+                    function () {
+                        var parent = $(this).parent();
+                        
+                        
+                        $(parent).find('.itemSelectableParent').each(function () {
+
+                            if ($(this).is(':visible')) {
+                                $(this).hide();
+                            }
+                            else {
+                                $(this).show();
+                            }
+                            
+                        });
+                        
+
+                            
+                        
+
+                    });
+            });
+        //Xml search for data functions:
+        function getDataByName(name) {
+              $(data).find('TreeViewItem').children().each(function () {
+                var elemName = ($(this).find('Name'));
+                if ($(elemName).text() == name) {
+                    
+                    var resultInfo = '';
+                    var parent = $(elemName).parent();
+                    $(parent).children().each(function () {
+                        if ($(this).text().length > 0 ) {
+                            var row = $(this).prop('tagName') + ': ' + $(this).text() + '\n';
+                        }
+                        else {
+                            var row = $(this).prop('tagName') + ': N/A' + '\n';
+                        }
+                    
+                    resultInfo += row;    
+                            
+                    })
+                    alert(resultInfo);
+                    
+                }
+            });
+            
+            
+            
+            
+            
+
+        }
+        //Gets data from console 
+        function getData() {
+            var elements = con.textContent.split('\n');
+            var vars = $(data).find('TreeViewItem');
+
+            elements.pop();
+            for (let i = 0; i < elements.length; i++) {
+                elements[i] = elements[i].substring(elements[i].lastIndexOf(':') + 1).trim();
+
+            }
+            let datas = new Set();
+            for (let j = 0; j < vars.length; j++) {
+                var text = $(vars[j]).text();
+                for (let k = 0; k < elements.length; k++) {
+                    if (text.includes(elements[k])) {
+                        datas.add(vars[j]);
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                
+            }
+
+            //initializing aml doc;
+            function generateAML() {
+                var writer = new XMLWriter('UTF-8', '1.0');
+                // Header of AML file
+                writer.writeStartDocument(true);
+                writer.writeElementString('CAEXFile');
+                writer.writeAttributeString('FileName', 'Model.aml');
+                writer.writeAttributeString('SchemaVersion', '2.15');
+                writer.writeAttributeString('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+                writer.writeEndElement();
+                writer.writeStartElement('AdditionalInformation');
+                writer.writeAttributeString('AutomationMLVersion', '3.0');
+                writer.writeEndElement();
+                writer.writeStartElement('AdditionalInformation');
+                writer.writeStartElement('WriterHeader');
+                writer.writeElementString('WriterName', 'Automation ML e.V.');
+                writer.writeElementString('WriterID', 'Automation ML e.V.');
+                writer.writeElementString('WriterVendor', 'Automation ML e.V.');
+                writer.writeElementString('WriterVendorURL', 'www.AutomationML.org');
+                writer.writeElementString('WriterVersion', '5.6.10.0');
+                writer.writeElementString('WriterRelease', '5.6.10.0');
+                writer.writeElementString('LastWritingDateTime', '2012-02-20');
+                writer.writeElementString('WriterProjectTitle', 'Testing');
+                writer.writeElementString('WriterProjectID', 'My Project');
+                writer.writeEndElement();
+                writer.writeEndElement();
+                var items = Array.from(datas);
+
+                // Data 
+                writer.writeStartElement('InstanceHierarchy');
+                writer.writeAttributeString('Name', 'Plant');
+                var counter = 1;
+                $(items).find('Data').each(function () {
+                    writer.writeStartElement('InternalElement');
+                    writer.writeAttributeString('Name', $(this).find('Name').text());
+                    writer.writeAttributeString('ID', counter);
+                    counter++;
+
+                    $(this.children).each(function () {
+
+                        writer.writeStartElement('Attribute');
+                        writer.writeAttributeString('Name', $(this).prop('tagName'));
+                        writer.writeElementString('Value', $(this).text());
+                        writer.writeEndElement();
+
+                    });
+                    writer.writeEndElement();
+
+
+                })
+                writer.writeEndElement();
+               
+
+                // Interface Classes
+                writer.writeStartElement('InterfaceClassLib');
+                writer.writeAttributeString('Name', 'Interfaces');
+
+                writer.writeElementString('Version', '0');
+
+
+                writer.writeEndElement();
+                writer.writeEndElement();
+                
+                // Role Classes
+                writer.writeStartElement('RoleClassLib');
+                writer.writeAttributeString('Name', 'Roles');
+
+                writer.writeElementString('Version', '0');
+
+
+                writer.writeEndElement();
+                writer.writeEndElement();
+
+                // Categories array
+                var headers = $(data).find('TreeViewItemHead');
+                var categories = Array();
+                $(headers).each(function () {
+                    var cat = $(this).attr('Category')
+                    if (cat != undefined) {
+                        
+                        categories.push(cat);
+                    }
+                    
+                });
+                console.log(categories);
+
+                // Unit Classes
+                writer.writeStartElement('SystemUnitClassLib');
+                writer.writeAttributeString('Name', 'Plant');
+                for (var c = 0; c < categories.length; c += 1) {
+                    writer.writeStartElement('SystemUnitClass');
+                    writer.writeAttributeString('Name', categories[c]);
+                    writer.writeElementString('Version', '0');
+                    for (var k = 0; k < items.length; k += 1) {
+                        var elem = $(whoIsParent(whoIsParent(items[k]))).attr('Text');
+                        if (elem == categories[c]) {
+
+                            writer.writeStartElement('InternalElement');
+                            writer.writeAttributeString('Name', $(items[k]).find('Name').text());
+                            writer.writeAttributeString('ID', (k).toString() + 'IE');
+                            writer.writeEndElement();
+
+
+                        };
+                    }
+                    writer.writeEndElement();
+                    
+                }
+                writer.writeEndElement();
+                // Attribute Types
+                
+                writer.writeStartElement('AttributeTypeLib');
+                writer.writeAttributeString('Name', 'Attributes');
+
+                writer.writeElementString('Version', '0');
+
+
+                
+                writer.writeEndElement();
+
+                writer.writeEndElement();
+              
+               
+
+               
+
+                
+                
+                writer.writeEndDocument();
+
+                var result = writer.flush();
+
+
+                var str = result.toString();
+                //replacing outdated declaration to prevent AML crashes;
+                str = str.replace('<?xml version="1.0" encoding="UTF-8" standalone="true" ?>', '<?xml version="1.0" encoding="UTF-8" ?>')
+                
+                //Download creation
+                var a = document.createElement('a');
+                a.href = window.URL.createObjectURL(new Blob([str]));
+                a.download = 'Model.aml';
+                a.click();
+                console.log(str);
+            }
+            generateAML();
+            
+            
+            
+        };
+        //Aml button logic;
+
+        var amlButt = document.getElementById('genAML');
+        amlButt.addEventListener('click', function () {
+            var text = con.textContent;
+            if (text != null && text != '') {
+
+                getData();
+            }
+            else {
+                alert('No info here');
+            }
+        });
+
+    },
+    error: function () {
+        console.log('Unexpected Error at AML button');
+    }
+    
+    
+
+})
+
+// Checks if current item is having any child elements;
+function isParent(value) {
+    var childrencount = $(value).children('TreeViewItemHead').length;
+    var childrenitemcount = $(value).children('TreeViewItem').length;
+    if ((childrencount > 0 || childrenitemcount > 0) && $(value).attr('Text') != 'Machinery') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function whoIsParent(value) {
+    return $(value).parent();
+}
+var counter = 0;
+//Returns the (user visible) name of selected element in the list;
+const con = document.getElementById('console');
+function itemSelected() {
+    
+    con.textContent += counter + ': Added: ' + $(this).parent().find('a').text() + '\n';
+    counter++;
+}
+//Clear button logic;
+var clearButt = document.getElementById('clearData');
+clearButt.addEventListener('click', function () {
+   
+    con.textContent = '';
+    con.focus();
+    counter = 0;
+})
+
+
+
+
+
+
+
+
