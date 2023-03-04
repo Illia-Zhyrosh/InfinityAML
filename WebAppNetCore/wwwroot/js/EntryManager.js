4f55cf6507ef23131927d4e9eeacd1e3ad7ec612
Illia-Zhyrosh <illia.zhyrosh@gmail.com>
2023-03-04 15:03:56 +0100

    4.03.2023

--- WebAppNetCore/wwwroot/js/EntryManager.js
+++ WebAppNetCore/wwwroot/js/EntryManager.js
@@ -3,8 +3,8 @@
     dataType: 'xml',
     cache: false,
     success: function (data) {
+        // Adding all available categories 
         var options = document.getElementById('categoryOptions');
-        var machineName = document.getElementById('machineName');
         var headers = $(data).find('TreeViewItemHead');
         var categories = Array();
         $(headers).each(function () {
@@ -22,24 +22,97 @@
             
             options.appendChild(selectable);
         }
+        // Buttons logic
 
         const okButt = document.getElementById('okButt');
         const cancelButt = document.getElementById('cancelButt');
         const rowButt = document.getElementById('rowAddButt');
+        // Reading user input
         okButt.addEventListener('click', function () {
-            console.log(options.value + '\n' + machineName.value);
+            var doc = document.getElementById('parentNode');
+            var parameterStrings = '';
+            $(doc).children().each(function () {
+                if ($(this).attr('role') == 'row' && $(this).attr('class') != 'buttonPanel') {
+                    var name,value;
+                    
+                    $(this).children().each(function () {
+                        
+                        if ($(this).prop('tagName') == 'A') {
+                            
+                            name = this.text;
+                        }
+                        else {
+
+                            
+                            value = this.value
+
+
+                        }
+                        
+                    });
+                    parameterStrings += `-${name}-${value}`;
+                }
+                else if ($(this).attr('class') == 'optionalData' && $(this).children().length > 0) {
+                    console.log('Additional options detected!');
+                    $(this).children().each(function () {
+                        var name, input1, input2, input3, value, units;
+                        var parameterRow;
+                        $(this).children().each(function () {
+                            if ($(this).prop('tagName') == 'SELECT') {
+                                name = this.value;
+                            }
+                            else if ($(this).prop('tagName') == 'INPUT') {
+                                if (input1 == undefined) {
+                                    input1 = this.value;
+                                }
+                                else if (input2 == undefined && input1 != undefined) {
+                                    input2 = this.value;
+                                }
+                                else {
+                                    input3 = this.value;
+                                }
+                            }
+                            else {
+                                if (value == undefined) {
+                                    value = this.text.replace(': ', ' ');
+                                }
+                                else {
+                                    units = this.text.replace(': ', ' ');;
+                                }
+                            }
+                        });
+                        if (name != 'Attribute') {
+                            parameterRow = `-${name}-${input1}`
+                        }
+                        else {
+                            parameterRow = `-${name}-${input1}-${value}-${input2}-${units}-${input3}`
+                        }
+                        parameterStrings += parameterRow;
+                    });
+                    
+                }
+                
+
+            });
+            console.log(parameterStrings);
+            // Creating XML appendage
+            var writer = new XMLWriter('UTF-8', '1.0');
+            var stringArr = (parameterStrings.split('-'));
+            console.log(stringArr);
+            
+            
         });
         cancelButt.addEventListener('click', function () {
             alert('Cancel!');
         });
+        // Adding custom options button
         rowButt.addEventListener('click', function () {
             var div = document.createElement('div');
             div.setAttribute('role', 'row');
-            var selectName = document.createElement('select');
-            var text = document.createElement('option');
-            text.innerText = 'optionName';
-            selectName.appendChild(text);
-            div.appendChild(selectName);
+            var optionName = document.createElement('input');
+            optionName.id = 'inputField';
+            optionName.setAttribute('required', 'required');
+            div.appendChild(optionName);
 
             var selectType = document.createElement('select');
             var typeClass = document.createElement('option');
@@ -52,12 +125,44 @@
             typeInterface.innerText = 'Interface';
             selectType.append(typeClass, typeAttribute, typeRole, typeInterface);
             div.appendChild(selectType);
+
+            var valueTag = document.createElement('a');
+            valueTag.text = 'Value: ';
+
+            var unitTag = document.createElement('a');
+            unitTag.text = 'Unit: ';
+            unitTag.className = 'unitTag';
+            var input = document.createElement('input');
+            input.setAttribute('required', 'required');
+            
+            input.disabled = true;
+
+            var input2 = document.createElement('input');
+            input2.setAttribute('required', 'required');
+            input2.disabled = true;
+            input2.id = 'optionInput';
+            div.appendChild(valueTag);
+            div.appendChild(input);
+            div.appendChild(input2);
+            div.appendChild(unitTag);
             
 
-            var input = document.createElement('input');
-            input.setAttribute('required','required');
-            div.appendChild(input);
             document.getElementById('optionalData').appendChild(div);
+            selectType.addEventListener('change', function () {
+                if (selectType.value == 'Attribute') {
+                    input.disabled = false;
+                    input2.disabled = false;
+                }
+                else {
+                    input.disabled = true;
+                    input2.disabled = true;
+                    input.value = '';
+                    input2.value = '';
+                }
+
+            });
+
+           
         })
 
     }
